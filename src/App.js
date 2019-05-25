d501faff16644140f9638a0c13f94db3e17aafcb
Hassnian <hassnianxd@gmail.com>
2019-05-25 14:09:50 +0200

    [ADD] colorhelper functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Palette from "./Palette";
 import seedColors from './seedColors'
+import { generatePalettes } from "./ColorHelpers";
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+    {console.log(generatePalettes(seedColors[3]))}
       <Palette {...seedColors[3]}/>
     </div>
   );
--- src/Palette.js
+++ src/Palette.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
-import ColorBox from './ColorBox'
-import './Palette.css'
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import "./Palette.css";
 export class Palette extends Component {
-    render() {
-        const colorsBoxes=this.props.colors.map(color=><ColorBox background={color.color} name={color.name}/>)
-        return (
-            <div className="Palette">
-               <div className="Palette-colors">
-{colorsBoxes}
-               </div>
-            </div>
-        )
-    }
+  render() {
+    const colorsBoxes = this.props.colors.map(color => (
+      <ColorBox background={color.color} name={color.name} />
+    ));
+    return (
+      <div className="Palette">
+        <div className="Palette-colors">{colorsBoxes}</div>
+      </div>
+    );
+  }
 }
 
-export default Palette
+export default Palette;
