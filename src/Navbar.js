25545a17d321b13c5032b7f0fc561b93b9ed3099
Hassnian <hassnianxd@gmail.com>
2019-05-26 10:53:30 +0200

    [ADD] snackbar added with style

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,35 +1,49 @@
 import React, { Component } from "react";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
-import './Navbar.css'
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
+import "./Navbar.css";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
+import { Icon } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+
+
 export class Navbar extends Component {
   constructor(props) {
-    super(props)
-    this.handleChange=this.handleChange.bind(this)
+    super(props);
+    this.handleFormatChange = this.handleFormatChange.bind(this);
+    this.closeSnackBar = this.closeSnackBar.bind(this);
     this.state = {
-       format:"hex"
-    }
+      format: "hex",
+      open:false
+    };
   }
-  handleChange(e){
-    this.setState({
-      format:e.target.value
-    },()=>{
-      this.props.handleChange(this.state.format)
-    })
-    
+  handleFormatChange(e) {
+    this.setState(
+      {
+        format: e.target.value,
+        open:true
+      },
+      () => {
+        this.props.handleChange(this.state.format);
+      }
+    );
+  }
+  closeSnackBar(){
+    this.setState({open:false})
   }
   render() {
-    const { level, changeLevel ,handleChange} = this.props;
-    const { format} = this.state;
+    const { level, changeLevel } = this.props;
+    const { format } = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
           <a href="#">reactcolorpicker</a>
         </div>
         <div className="slider-container">
-            <span>Level: {level}</span>
+          <span>Level: {level}</span>
           <div className="slider">
             <Slider
               defaultValue={level}
@@ -41,12 +55,24 @@ export class Navbar extends Component {
           </div>
         </div>
         <div className="select-container">
-        <Select onChange={this.handleChange} value={format}>
-          <MenuItem  value="hex">HEX -#ffffff</MenuItem>
-          <MenuItem value="rgb">RGB -rgb(255,255,255)</MenuItem>
-          <MenuItem value="rgba">RGBA -rgb(255,255,255,1)</MenuItem>
-        </Select>
+          <Select onChange={this.handleFormatChange} value={format}>
+            <MenuItem value="hex">HEX -#ffffff</MenuItem>
+            <MenuItem value="rgb">RGB -rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA -rgb(255,255,255,1)</MenuItem>
+          </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={this.state.open}
+          autoHideDuration={3000}
+          onClose={this.closeSnackBar}
+          message={<span id="message-id">Format changed to {format.toUpperCase()}</span>}
+          action={[
+            <IconButton onClick={this.closeSnackBar} color="inherit" key="close" >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </header>
     );
   }
