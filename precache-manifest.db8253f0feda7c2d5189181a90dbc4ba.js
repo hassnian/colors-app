727b1f24d4ad3e14c5a1a6b95eddf74468d6ca97
Hassnian <hassnianxd@gmail.com>
2019-06-08 16:23:04 +0200

    Updates

---
+++ precache-manifest.db8253f0feda7c2d5189181a90dbc4ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d32efabf1cf9fb2c2c75254af077bf94",
+    "url": "/colors-app/index.html"
+  },
+  {
+    "revision": "1d040a192db33c31a4c0",
+    "url": "/colors-app/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "f6ebcf09cabd30371014",
+    "url": "/colors-app/static/css/main.b2fe2d00.chunk.css"
+  },
+  {
+    "revision": "1d040a192db33c31a4c0",
+    "url": "/colors-app/static/js/2.59cea498.chunk.js"
+  },
+  {
+    "revision": "f6ebcf09cabd30371014",
+    "url": "/colors-app/static/js/main.3d51dda2.chunk.js"
+  },
+  {
+    "revision": "658f8f8a04f3c7d8f86e",
+    "url": "/colors-app/static/js/runtime~main.7b7bc380.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/colors-app/precache-manifest.c3909ea322e8c81206f90cfe027bbc6c.js"
+  "/colors-app/precache-manifest.db8253f0feda7c2d5189181a90dbc4ba.js"
 );
 
 self.addEventListener('message', (event) => {
